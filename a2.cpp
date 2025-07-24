27782099a4272d653073e27e828dda0eeedb3e59
Nayeem Ahammed <nayeem.ahammed01@gmail.com>
2025-07-24 11:08:04 +0600

    Create a2.cpp

---
+++ a2.cpp
@@ -0,0 +1,30 @@
+#ifndef ONLINE_JUDGE
+#include "DEBUG.cpp";
+#else
+#include <bits/stdc++.h>
+#define dbg(...)
+#endif
+using namespace std;
+
+#define vin(a) for(auto &e : a) cin>>e;
+#define vout(a) for(auto it : a) cout<<it<<' ';cout<<'\n';
+#define all(v) v.begin(), v.end()
+#define rep(i,n) for(int i=0; i<n; i++)
+#define Bool(x) cout<<(x ? "YES\n" : "NO\n");
+using ll = long long; using pii = pair<int, int>;
+using pll = pair<ll, ll>; using vi = vector<int>;
+template<class T> using V = vector<T>;
+
+void solution(int t) {
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ll tc = 1, t = 1; cin >> tc;
+    while (tc--) {
+        solution(t++);
+    }
+    return 0;
+}
